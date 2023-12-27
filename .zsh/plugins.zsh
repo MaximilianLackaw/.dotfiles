d7aaf7fb3fd52f84ccce81fae2a520c556c35f73
Maximilian Lackaw <mlackaw@gmail.com>
2023-12-27 14:53:13 +0100

    Optimize plugin load

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,7 +1,26 @@
+# Settings
+
+## zsh-history-substring-search
+# bindkey '^[[A' history-substring-search-up
+# bindkey '^[[B' history-substring-search-down
+
+# works on wsl
+# bindkey "$terminfo[kcuu1]" history-substring-search-up
+# bindkey "$terminfo[kcud1]" history-substring-search-down
+
+# zsh-autosuggestions
+
+ZSH_AUTOSUGGEST_STRATEGY=(history)
+
+# nvm
+export NVM_COMPLETION=true
+export NVM_LAZY_LOAD=false
+export NVM_AUTO_USE=true
+
 # Plugin "Manager"
 
 # where do you want to store your plugins?
-ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$ZSH}/plugins}
+ZPLUGINDIR=$HOME/.zsh/plugins
 
 # get zsh_unplugged and store it with your other plugins
 if [[ ! -d $ZPLUGINDIR/zsh_unplugged ]]; then
@@ -11,33 +30,25 @@ source $ZPLUGINDIR/zsh_unplugged/zsh_unplugged.zsh
 
 # make list of the Zsh plugins you use
 repos=(
+  zsh-users/zsh-syntax-highlighting # always before history-substring-search
+  romkatv/zsh-defer
   # romkatv/powerlevel10k
   zsh-users/zsh-completions
   rupa/z
   djui/alias-tips
   # olets/zsh-abbr # makes no sens together with alias-tips
-  zsh-users/zsh-syntax-highlighting # always before history-substring-search
   zsh-users/zsh-history-substring-search
   unixorn/fzf-zsh-plugin # similar to history-substring-search
   zsh-users/zsh-autosuggestions
+  lukechilds/zsh-nvm
 )
 
 # now load your plugins
 plugin-load $repos
 
 # load non git managed plugins
-[[ -f $ZPLUGINDIR/sudo.zsh ]] && source $ZPLUGINDIR/sudo.zsh
-[[ -f $ZPLUGINDIR/dirhistory.zsh ]] && source $ZPLUGINDIR/dirhistory.zsh # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/dirhistory/README.md
-[[ -f $ZPLUGINDIR/web-search.zsh ]] && source $ZPLUGINDIR/web-search.zsh # https://github.com/sineto/web-search/tree/master
-[[ -f $ZPLUGINDIR/copybuffer.zsh ]] && source $ZPLUGINDIR/copybuffer.zsh  # https://github.com/guillaumeboehm/zsh-copybuffer/tree/master
-
-
-# Settings
-## zsh-history-substring-search
-# bindkey '^[[A' history-substring-search-up
-# bindkey '^[[B' history-substring-search-down
-
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-
+[[ -f $ZPLUGINDIR/sudo.zsh ]] && zsh-defer . $ZPLUGINDIR/sudo.zsh
+[[ -f $ZPLUGINDIR/dirhistory.zsh ]] && zsh-defer . $ZPLUGINDIR/dirhistory.zsh # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/dirhistory/README.md
+[[ -f $ZPLUGINDIR/web-search.zsh ]] && zsh-defer . $ZPLUGINDIR/web-search.zsh # https://github.com/sineto/web-search/tree/master
+[[ -f $ZPLUGINDIR/copybuffer.zsh ]] && zsh-defer . $ZPLUGINDIR/copybuffer.zsh  # https://github.com/guillaumeboehm/zsh-copybuffer/tree/master
 
--- .zshrc
+++ .zshrc
@@ -1,6 +1,5 @@
 export ZSH=$HOME/.zsh
 
-
 # [[ -f $ZSH/p10k.zsh ]] && source $ZSH/p10k.zsh # if disabled, also disabled plugin in `$ZSH/plugins.zsh`
 [[ -f $ZSH/aliases.zsh ]] && source $ZSH/aliases.zsh
 [[ -f $ZSH/history.zsh ]] && source $ZSH/history.zsh
@@ -10,11 +9,6 @@ export ZSH=$HOME/.zsh
 # Fix arrow keys
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
-
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Starship (Best end of zshrc)
 # export STARSHIP_CONFIG=~/.zsh/starship.toml
