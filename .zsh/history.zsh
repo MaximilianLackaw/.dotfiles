97c0dd11fd846cd8e7b071c8cadabba020abe329
Maximilian Lackaw <mlackaw@gmail.com>
2024-07-28 20:09:47 +0200

    Extend history config

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -1,8 +1,11 @@
 ### ---- History Configuration -----------------------------------
-HISTSIZE=1000000               #How many lines of history to keep in memory
+HISTSIZE=100000               #How many lines of history to keep in memory
 HISTFILE=$HOME/.zsh_history     #Where to save history to disk
-SAVEHIST=1000000               #Number of history entries to save to disk
+SAVEHIST=100000               #Number of history entries to save to disk
+HISTDUP=erase
 setopt appendhistory
-setopt share_history
-setopt hist_ignore_all_dups
+setopt sharehistory
 setopt hist_ignore_space #ignores all commands starting with a blank space! Usefull for passwords
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
